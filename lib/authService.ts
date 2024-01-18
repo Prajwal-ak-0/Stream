fc773c3083bb6d59e0bbbcd3afaa0e47ab8dad8f
Prajwal A K <prajwalhassan7@gmail.com>
2024-01-18 23:26:11 +0530

    Dashboard Page for Admin Completed...

--- lib/authService.ts
+++ lib/authService.ts
@@ -1,21 +1,45 @@
 import { currentUser } from "@clerk/nextjs"
 import { db } from "./db";
 
-export const getSelf =async () => {
+export const getSelf = async () => {
     const self = await currentUser();
 
-    if(!self) {
+    if (!self) {
         throw new Error("Unauthorized")
     }
 
     const user = await db.user.findUnique({
-        where:{
+        where: {
             externalUserId: self.id
         }
     })
 
-    if(!user) {
+    if (!user) {
         throw new Error("No user found")
+    }
+
+    return user;
+}
+
+export const getSelfByUsername = async (username: string) => {
+    const self = await getSelf();
+
+    if (!self || !self.username) {
+        throw new Error("Unauthorized")
+    }
+
+    const user = await db.user.findUnique({
+        where: {
+            username
+        }
+    })
+
+    if (!user) {
+        throw new Error("No user found")
+    }
+
+    if (user.username !== self.username) {
+        throw new Error("Unauthorized")
     }
 
     return user;
