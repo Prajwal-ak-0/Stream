b4029aa0743157fc33bdd166a3448c2bfad8e728
Prajwal A K <prajwalhassan7@gmail.com>
2024-01-21 09:34:10 +0530

    Description Section For Host / Profile Section Completed

--- components/streamPlayer/index.tsx
+++ components/streamPlayer/index.tsx
@@ -1,53 +1,101 @@
 "use client";
 
-import { useViewerToken } from "@/hooks/useViewerToken";
 import { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from "@livekit/components-react";
-import { Video } from "./Video";
-import { Chat } from "./Chat";
+
 import { cn } from "@/lib/utils";
 import { useChatSidebar } from "@/hooks/useChatSidebar";
+import { useViewerToken } from "@/hooks/useViewerToken";
+
+import { InfoCard } from "./InfoCard";
+import { AboutCard } from "./AboutCard";
+import { ChatToggle } from "./ChatToggle";
+import { Chat, ChatSkeleton } from "./Chat";
+import { Video, VideoSkeleton } from "./Video";
+import { Header, HeaderSkeleton } from "./Header";
+
+type CustomStream = {
+  id: string;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+  isLive: boolean;
+  thumbnailUrl: string | null;
+  name: string;
+};
+
+type CustomUser = {
+  id: string;
+  username: string;
+  bio: string | null;
+  stream: CustomStream | null;
+  imageUrl: string;
+  _count: { followedBy: number }
+};
 
 interface StreamPlayerProps {
-  user: User & {
-    stream: Stream | null;
-  };
-  stream: Stream;
+  user: CustomUser;
+  stream: CustomStream;
   isFollowing: boolean;
 }
-const StreamPlayer = ({
+
+export const StreamPlayer = ({
   user,
   stream,
-  isFollowing,
+  isFollowing
 }: StreamPlayerProps) => {
   const {
     token,
     name,
     identity,
   } = useViewerToken(user.id);
-
   const { collapsed } = useChatSidebar((state) => state);
 
   if (!token || !name || !identity) {
-    return (
-      <div className="">
-        Can not watch stream
-      </div>
-    );
+    return <StreamPlayerSkeleton />
   }
+
   return (
     <>
+      {collapsed && (
+        <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+          <ChatToggle />
+        </div>
+      )}
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+        className={cn(
+          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+          collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+        )}
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video
             hostName={user.username}
             hostIdentity={user.id}
           />
-
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
+          <InfoCard
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            name={stream.name}
+            thumbnailUrl={stream.thumbnailUrl}
+          />
+          <AboutCard
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            bio={user.bio}
+            followedByCount={user._count.followedBy}
+          />
         </div>
         <div
           className={cn(
@@ -67,7 +115,19 @@ const StreamPlayer = ({
         </div>
       </LiveKitRoom>
     </>
+  );
+};
+
+export const StreamPlayerSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+      <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+        <VideoSkeleton />
+        <HeaderSkeleton />
+      </div>
+      <div className="col-span-1 bg-background">
+        <ChatSkeleton />
+      </div>
+    </div>
   )
 }
-
-export default StreamPlayer
